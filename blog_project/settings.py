6251b46fc4c25fc0360f29935862609552038ce7
Anjali33333 <anjalisreekumar0304@gmail.com>
2025-03-24 23:14:45 +0530

    iniitial commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if "RENDER" in os.environ:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_URL = "/static/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
